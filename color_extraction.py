e52f723e09176dd45da082957bd734606a7df48e
cgkim412 <hermercur@gmail.com>
2022-12-04 20:25:22 +0900

    sort result by cluster size

--- color_extraction.py
+++ color_extraction.py
@@ -1,15 +1,8 @@
-from skimage.color import rgb2lab, rgb2xyz, xyz2lab, lab2rgb
-from skimage.io import imread, imshow
-from PIL import Image
 import numpy as np
+from PIL import Image
+from skimage.color import lab2rgb, rgb2lab
+from skimage.io import imshow
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-from typing import NamedTuple, Tuple
-
-
-# class Color(NamedTuple):
-#     rgb: Tuple[int, int, int]
-#     lab: Tuple[float, float, float]
 
 
 def float2int(x):
@@ -27,7 +20,10 @@ def extract_colors(img: Image.Image, n_colors=8, downsample_size=128):
     kms = KMeans(n_clusters=n_colors, n_init=3, random_state=9)
     kms.fit(X)
 
-    lab_centers = kms.cluster_centers_
+    _, counts = np.unique(kms.labels_, return_counts=True)
+    order = counts.argsort()[::-1]
+
+    lab_centers = kms.cluster_centers_[order].copy()
     rgb_centers = float2int(lab2rgb(lab_centers))
     return dict(rgb=rgb_centers, lab=lab_centers)
 
